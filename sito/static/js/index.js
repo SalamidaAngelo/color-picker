9e826aa3b2b67d05ce30c3d05dbcf4a2745d95c3
SalamidaAngelo <115226403+SalamidaAngelo@users.noreply.github.com>
2022-11-25 17:07:06 +0100

    upgrade button

---
+++ sito/static/js/index.js
@@ -0,0 +1,25 @@
+function ChangeColor1(){
+
+    
+    document.body.style.backgroundColor=" red";
+}
+function ChangeColor2(){
+    
+    document.body.style.backgroundColor=" #59BC98";
+}
+function ChangeColor3(){
+    
+    document.body.style.backgroundColor=" #2594D2";
+}
+function ChangeColor4(){
+    
+    document.body.style.backgroundColor=" #72BC59";
+}
+function ChangeColor5(){
+    
+    document.body.style.backgroundColor=" #BC59A0";
+}
+function ChangeColor6(){
+    
+    document.body.style.backgroundColor=" #DCF73C";
+}
